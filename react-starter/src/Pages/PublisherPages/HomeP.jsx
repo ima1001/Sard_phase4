ed0e7fd3571172175cdeea7416c28067e3c43e17
Layan-2004 <layanalasmari25@gmail.com>
2026-03-29 21:43:05 +0300

    Publisher

--- react-starter/src/Pages/PublisherPages/HomeP.jsx
+++ react-starter/src/Pages/PublisherPages/HomeP.jsx
@@ -2,25 +2,28 @@ import CommunitytCard from "../../components/Card.jsx";
 import communities from "../../../communityData.json";
 
 function HomeP() {
-    return (
-        <div className="home">
-            <h1>Welcome to SARD Platform</h1>
-        <div className="communities">
-            <h2>Communities</h2>
-            <div>
-                {communities.map((community) => (
-                    <CommunitytCard
-                    key={community.id}
-                    title={community.title}
-                    text={community.text}
-                    imageSrc={community.imageSrc}
-                    onClick={() => alert(community.title)}
-                    />
-                ))}
-            </div>
+  return (
+    <div className="home">
+      <div className="home-top">
+        <h1>Welcome to SARD Platform</h1>
+      </div>
+
+      <div className="home-bottom">
+        <h2>Communities</h2>
+        <div className="communities-container">
+          {communities.map((community) => (
+            <CommunitytCard
+              key={community.id}
+              title={community.title}
+              text={community.text}
+              imageSrc={community.imageSrc}
+              onClick={() => alert(community.title)}
+            />
+          ))}
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default HomeP;
