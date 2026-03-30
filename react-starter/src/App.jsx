a0b2e4710d1559a5b76529a0b68486c4803599b1
Layan-2004 <layanalasmari25@gmail.com>
2026-03-30 12:24:55 +0300

    SideBare update

--- react-starter/src/App.jsx
+++ react-starter/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+/*import { Routes, Route } from "react-router-dom";
 import SideBar from "./components/SideBar";
 
 import HomeE from "./Pages/EditorPages/HomeE";
@@ -15,6 +15,60 @@ function App() {
   return (
     <div style={{ display: "flex" }}>
       <SideBar />
+      <div style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/HomeE" element={<HomeE />} />
+          <Route path="/ProjectsE" element={<ProjectsE />} />
+          <Route path="/NotificationE" element={<NotificationE />} />
+
+          <Route path="/HomeP" element={<HomeP />} />
+          <Route path="/CommunityP" element={<CommunityP />} />
+          <Route path="/NotificationP" element={<NotificationP />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;*/
+
+import { Routes, Route, useLocation } from "react-router-dom";
+import SideBar from "./components/SideBar";
+
+import HomeE from "./Pages/EditorPages/HomeE";
+import ProjectsE from "./Pages/EditorPages/ProjectsE";
+import NotificationE from "./Pages/EditorPages/NotificationE";
+
+import HomeP from "./Pages/PublisherPages/HomeP";
+import CommunityP from "./Pages/PublisherPages/CommunityP";
+import NotificationP from "./Pages/PublisherPages/NotificationP";
+
+import "./App.css";
+
+function App() {
+  const location = useLocation();
+  const isPublisher = location.pathname.includes("P");
+
+  return (
+    <div style={{ display: "flex" }}>
+      <SideBar
+        role={isPublisher ? "PUBLISHER" : "EDITOR"}
+        name={isPublisher ? "Alex (Penguin House)" : "Andrew Smith"}
+        homeLink={isPublisher ? "/HomeP" : "/HomeE"}
+        notificationLink={isPublisher ? "/NotificationP" : "/NotificationE"}
+        projectLinks={
+          isPublisher
+            ? [
+                { name: "Book1", link: "/CommunityP" },
+                { name: "Book2", link: "/CommunityP" },
+              ]
+            : [
+                { name: "Book1", link: "/ProjectsE" },
+                { name: "Book2", link: "/ProjectsE" },
+              ]
+        }
+      />
+
       <div style={{ flex: 1 }}>
         <Routes>
           <Route path="/HomeE" element={<HomeE />} />
--- react-starter/src/components/SideBar.jsx
+++ react-starter/src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+/*import { NavLink } from "react-router-dom";
 import logo from "../assets/logoD.png";
 
 function SideBar() {
@@ -16,6 +16,74 @@ function SideBar() {
         </nav>
         </div>
     );
+}
+
+export default SideBar;*/
+
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import logo from "../assets/logoD.png";
+import {
+  House,
+  Pencil,
+  Bell,
+  ChevronDown,
+  ChevronUp,
+  PersonCircle
+} from "react-bootstrap-icons";
+
+function SideBar({ role, name, homeLink, notificationLink, projectLinks = [] }) {
+  const [showProjects, setShowProjects] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <div className="logo-container">
+        <img src={logo} alt="Sard Logo" className="logo" />
+      </div>
+
+      <div className="profile-box">
+        <PersonCircle className="profile-icon" />
+        <div>
+          <p className="role-text">{role}</p>
+          <p className="name-text">{name}</p>
+        </div>
+      </div>
+
+      <nav>
+        <NavLink to={homeLink} className="nav-item">
+          <div className="nav-left">
+            <House className="nav-icon" />
+            Home
+          </div>
+        </NavLink>
+
+        <div className="nav-item project-toggle" onClick={() => setShowProjects(!showProjects)}>
+          <div className="nav-left">
+            <Pencil className="nav-icon" />
+            Projects
+          </div>
+          {showProjects ? <ChevronUp /> : <ChevronDown />}
+        </div>
+
+        {showProjects && (
+          <div className="dropdown-menu-projects">
+            {projectLinks.map((project, index) => (
+              <NavLink key={index} to={project.link} className="project-link">
+                {project.name}
+              </NavLink>
+            ))}
+          </div>
+        )}
+
+        <NavLink to={notificationLink} className="nav-item">
+          <div className="nav-left">
+            <Bell className="nav-icon" />
+            Notification
+          </div>
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 
 export default SideBar;
