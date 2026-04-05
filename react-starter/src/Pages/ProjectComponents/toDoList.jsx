d21ceb9a79a585da8a15a48c2177dd30695a3309
NabaAl-Antaif <s202278160@kfupm.edu.sa>
2026-04-05 10:58:37 +0300

    Add task function

--- react-starter/src/Pages/ProjectComponents/toDoList.jsx
+++ react-starter/src/Pages/ProjectComponents/toDoList.jsx
@@ -42,9 +42,35 @@ function ToDoList() {
         (tasks.filter((t) => t.status === "Completed").length / tasks.length) * 100;
 
     const [message, setMessage] = useState(null);
+    const [showAddForm, setShowAddForm] = useState(false);
     
-    function handleAddTask() {
-        
+    function AddTaskForm() {
+        return(
+            <div className="add-task-form">
+                <form action="#" method="post">
+                    <label for="taskName">Task:</label>
+                    <input type="text" id="taskName" placeholder="Enter task name: " />
+                    <label for="status">Status:</label>
+                    <select id="status">
+                        <option value="Proposed">Proposed</option>
+                        <option value="In progress">In progress</option>
+                        <option value="In review">In review</option>
+                        <option value="Completed">Completed</option>
+                    </select>
+                    <label for="lastUpdate">Last Update:</label>
+                    <input type="date" id="lastUpdate" placeholder="Enter date of last update: "/>
+                    <label for="deadline">Deadline:</label>
+                    <input type="date" id="deadline" placeholder="Enter deadline date: "/>
+                    <label for="Author">Author:</label>
+                    <select id="Author">
+                        <option value="Author (yoy)">Proposed</option>
+                        <option value="Editor">In progress</option>
+                        <option value="Reviewer">In review</option>
+                    </select>
+                    <button type="submit">Add</button>
+                </form>
+            </div>
+        );
     }
     function handlePublishEditors() {
         setMessage(<MessageCard type="success" text="Published for Editors successfully" />);
@@ -64,9 +90,10 @@ function ToDoList() {
             <div className="header">
                 <h3>Todo List</h3>
                 <div className="add-task">
-                    <button onClick={handleAddTask}>+ Add</button>
+                    <button onClick={() => setShowAddForm(true)}>+ Add</button>
                 </div>
             </div>
+            {showAddForm && <AddTaskForm />}
             <table>
                 <thead>
                     <tr>
