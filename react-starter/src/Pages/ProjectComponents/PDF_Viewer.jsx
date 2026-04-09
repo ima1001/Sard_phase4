c9da015cd35157945f698efa1c6cca8668a9b0fa
NabaAl-Antaif <s202278160@kfupm.edu.sa>
2026-04-09 23:10:13 +0300

    pdf viewer edit

--- react-starter/src/Pages/ProjectComponents/PDF_Viewer.jsx
+++ react-starter/src/Pages/ProjectComponents/PDF_Viewer.jsx
@@ -1,17 +1,52 @@
-import { PDFViewer } from '@embedpdf/react-pdf-viewer';
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
 
+pdfjs.GlobalWorkerOptions.workerSrc =
+    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.296/pdf.worker.min.js";
 
-function pdfViewer({ filePath }) {
-    return(
-        <div style={{ height: '600px' }}> 
-            <PDFViewer 
-                style={{ height: '100%' }}
-                config={{
-                    src: filePath,
-                    theme: { preference: 'light' }
-                }}
-            />
+function PDF_Viewer({ pdfUrl }) {
+
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    const onLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+        setPageNumber(1);
+    };
+
+    return (
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <Document
+            file={pdfUrl}
+            onLoadSuccess={onLoadSuccess}
+            onLoadError={(err) => console.error("PDF load error:", err)}
+        >
+            <Page pageNumber={pageNumber} width={600} />
+        </Document>
+
+        {numPages && (
+            <div style={{ marginTop: "10px" }}>
+            <button
+                onClick={() => setPageNumber(pageNumber - 1)}
+                disabled={pageNumber <= 1}
+            >
+                Previous
+            </button>
+
+            <span style={{ margin: "0 10px" }}>
+                Page {pageNumber} of {numPages}
+            </span>
+
+            <button
+                onClick={() => setPageNumber(pageNumber + 1)}
+                disabled={pageNumber >= numPages}
+            >
+                Next
+            </button>
+            </div>
+        )}
         </div>
     );
 }
-export default pdfViewer;
+
+export default PDF_Viewer;
