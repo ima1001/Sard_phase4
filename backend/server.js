38712ee429e1e3f9b72628b2dcbea0fe0db48bb5
Layan19-2004 <s202223720@kfupm.edu.sa>
2026-05-01 01:09:25 +0300

    Connecting with MongoDB

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// connect MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
