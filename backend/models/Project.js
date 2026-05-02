f9423d28f10c77a3a95e784853897ea50a4cf217
Fatimah <s202248340@kfupm.edu.sa>
2026-05-02 23:32:18 +0300

    Update communityNames field in project schema to store a single Community reference

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -5,7 +5,7 @@ const projectSchema = new mongoose.Schema({
     description: { type: String },
     numAuthors: { type: Number, required: true },
     accessibility: { type: String, required: true },
-    communityNames: [{ type: mongoose.Schema.Types.ObjectId, ref: "Community"}],
+    communityNames: { type: mongoose.Schema.Types.ObjectId},
     createdAt: { type: Date, default: Date.now }
 });
 
